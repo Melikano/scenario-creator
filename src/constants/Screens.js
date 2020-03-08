d14d17e7d1518076da443e1a687edc7fc7fd3c9c
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2020-03-08 11:34:59 +0330

    initiate draw state machine screen

---
+++ src/constants/Screens.js
@@ -0,0 +1,8 @@
+//@flow
+const Screens = {
+  profile: 'PROFILE_SCREEN',
+  addName: 'ADD_NAME_SCREEN',
+  chooseThings: 'CHOOSE_THINGS_SCREEN',
+  drawStateMachine: 'DRAW_STATE_MACHINE_SCREEN',
+};
+export default Screens;
--- src/navigation/NewScenarioStack.js
+++ src/navigation/NewScenarioStack.js
@@ -3,14 +3,23 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import AddNameScreen from '../screens/AddNameScreen';
 import ChooseThingsScreen from '../screens/ChooseThingsScreen';
+import DrawStateMachineScreen from '../screens/ChooseThingsScreen';
+import Screens from '../constants/Screens';
 
 const Stack = createStackNavigator();
 
 const NewScenarioStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="AddName" component={AddNameScreen} />
-      <Stack.Screen name="ChooseThings" component={ChooseThingsScreen} />
+      <Stack.Screen name={Screens.addName} component={AddNameScreen} />
+      <Stack.Screen
+        name={Screens.chooseThings}
+        component={ChooseThingsScreen}
+      />
+      <Stack.Screen
+        name={Screens.drawStateMachine}
+        component={DrawStateMachineScreen}
+      />
     </Stack.Navigator>
   );
 };
